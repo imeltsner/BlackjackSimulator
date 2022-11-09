8608cc231cc00261e83aa6dbac017d4fd43b5118
isaac meltsner <itmeltsner@dons.usfca.edu>
2022-11-08 22:19:50 -0800

    player object now directly connects to server

--- Blackjack.java
+++ Blackjack.java
@@ -4,14 +4,13 @@ import java.util.Arrays;
 //A program to play a game of blackjack computer vs computer
 public class Blackjack {   
     public static void main(String[] args) {
-        String IpAddress = args[0];
-        String IpPort = args[1];
+        String IP = args[0];
+        String PORT = args[1];
         String command = new String();
-        Game game = new Game(IpAddress, IpPort);
-        Player myPlayer = new HiLo();
+        Player myPlayer = new HiLo(IP, PORT);
         while (true) {
             try { //get command from dealer
-                command = game.read();
+                command = myPlayer.read();
             }
             catch (IOException e) {
                 continue;
@@ -19,7 +18,7 @@ public class Blackjack {
             String[] commandArray = myPlayer.processCommand(command);
             if (commandArray[0].equals("login")) { //login
                 try {
-                    game.write("imeltsner:isaac");
+                    myPlayer.write("imeltsner:isaac");
                 }
                 catch (IOException e) {
                     System.out.println("ERROR");
@@ -27,7 +26,7 @@ public class Blackjack {
             } else if (commandArray[0].equals("bet")) { //place bet
                 if (commandArray.length == 3) { //no cards seen -> bet 1
                     try {
-                        game.write("bet:1");
+                        myPlayer.write("bet:1");
                         myPlayer.subtractMoney(1);
                     }
                     catch (IOException e) {
@@ -38,7 +37,7 @@ public class Blackjack {
                     int aBet = myPlayer.placeBet(cards);
                     String myBet = Integer.toString(aBet);
                     try {
-                        game.write("bet:" + myBet);
+                        myPlayer.write("bet:" + myBet);
                     }
                     catch (IOException e) {
                         System.out.println("ERROR");
@@ -52,7 +51,7 @@ public class Blackjack {
                     System.out.println("Hand value is " + myPlayer.calculateHandValue(myHand));
                     System.out.println("My cards are " + myHand);
                     System.out.println("My action is " + myAction);
-                    game.write(myAction);
+                    myPlayer.write(myAction);
                 }
                 catch (IOException e) {
                     System.out.println("ERROR");
@@ -61,7 +60,7 @@ public class Blackjack {
                 System.out.println(command);
             } else if (commandArray[0].equals("done")) { //end game
                 System.out.println(commandArray[1]);
-                game.endGame();
+                myPlayer.endGame();
                 break;
             }
         }
